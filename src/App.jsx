1739769152e1569a81eb097b41cdfa80c1b4da3d
temycodes <temmy1345@gmail.com>
2026-03-31 16:26:16 +0100

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import AppLayout from "./ui/AppLayout";
+import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='landing' element={<LandingPage />} />
+        <Route element={<AppLayout />}>
+          <Route path='home' element={<Home />} />
+        </Route>
+        <Route path='login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
